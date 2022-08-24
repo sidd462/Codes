042ecbacdb52251b29e845829d120c48c8229f8e
sidd462 <34214713+sidd462@users.noreply.github.com>
2022-08-25 01:18:16 +0530

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+
+Node *removeDuplicates(Node *head)
+{Node * temp=head;
+ 
+    if(temp==NULL and temp->next==NULL)
+    return NULL;
+ 
+ if(temp->data==(temp->next)->data && temp->next!=NULL)
+     temp->next=temp->next->next;
+ Node* ans=removeDuplicates(head->next);
+
+ return ans;
+}
+
+int main(){
+
+   
+}
